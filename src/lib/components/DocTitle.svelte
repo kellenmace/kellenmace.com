e09a48108dea5bd3e0f334a4326900705ef41ca6
Kellen Mace <kellen.mace@gmail.com>
2025-01-29 22:45:01 -0500

    Fix blog post document titles

--- src/lib/components/DocTitle.svelte
+++ src/lib/components/DocTitle.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	let { pageTitle } = $props();
 
-	let docTitle = $derived(() => (pageTitle ? `${pageTitle} | Kellen Mace` : 'Kellen Mace'));
+	let docTitle = $derived(pageTitle ? `${pageTitle} | Kellen Mace` : 'Kellen Mace');
 </script>
 
 <svelte:head>
